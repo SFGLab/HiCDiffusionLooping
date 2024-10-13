ec7a4905741498e130a998fc613bb221960d11ff
m10an <vn.mlshk@gmail.com>
2024-10-13 11:41:35 +0200

    Updated pipeline steps

--- scripts/data_pipeline.sh
+++ scripts/data_pipeline.sh
@@ -1,2 +1,9 @@
-python hicdifflib/data.py delly_call 4DNFIG85Y8QS:v0
-python hicdifflib/data.py vcf_consensus 4DNFIG85Y8QS.delly.vcf.gz:latest
+# debug using small bam file
+# python hicdifflib/data.py delly_call 4DNFIG85Y8QS:v0
+# python hicdifflib/data.py vcf_consensus 4DNFIG85Y8QS.delly.vcf.gz:latest
+
+python hicdifflib/data.py delly_call 4DNFI1GNQM8L:v0
+python hicdifflib/data.py vcf_consensus 4DNFI1GNQM8L.delly.vcf.gz:latest
+# MA0139.2.meme:v0
+python hicdifflib/data.py fimo  # GRCh38-reference-genome
+python hicdifflib/data.py fimo --sequence 4DNFIG85Y8QS.delly.vcf.fa:v0
